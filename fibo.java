cd523e2226caab6090be2d3d254c98f1c746124f
Alan Cao <alannesta@sina.com>
2015-08-02 16:46:33 -0400

    cache

--- fibo.java
+++ fibo.java
@@ -1,14 +1,26 @@
+import java.util.HashMap;
+
 public class Fibonacci {
+	
+	private HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+	
 	public int fibo(int n) {
-		if (n == 0 || n == 1) {
+		if (cache.get(n) != null) {
+			return cache.get(n);
+		}
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
 			return 1;
 		}
-		return fibo(n-1) + fibo(n-2);
+		this.cache.put(n, fibo(n-1) + fibo(n-2));
+		return cache.get(n);
 	}
 	public static void main(String[] args) {
 		Fibonacci fibo = new Fibonacci();
 		long timeFlag = System.currentTimeMillis();
-		System.out.println(fibo.fibo(43));
+		System.out.println(fibo.fibo(31));
 		System.out.println("Time elapsed: " + (System.currentTimeMillis() - timeFlag) + "ms");
 	}
 }
