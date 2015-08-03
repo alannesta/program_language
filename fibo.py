98eefde89c402fb8c46c6b30bdbe90a4a0671011
Alan Cao <alannesta@sina.com>
2015-08-02 23:04:43 -0400

    cache for ruby and python

--- fibo.py
+++ fibo.py
@@ -1,12 +1,24 @@
 import time
+
+#cache = {}	# python dictionary
+cache = dict()
+
 def fibo(n):
-	if (n == 1 or n == 0):
+
+	if (n in cache):	# if (cache[n] is not None) will throw an exception in python
+		return cache[n]
+	if (n == 1):
 		return 1
-	return fibo(n-1) + fibo(n-2)
+	if (n == 0):
+		return 0
+	
+	cache[n] = fibo(n-1) + fibo(n-2)
+	return cache[n]
+
 
 def main():
 	now = time.time()
-	print(fibo(35))
+	print(fibo(41))
 	print 'Time elapsed: {0} milli seconds'.format((time.time() - now)*1000)
 
 if __name__ == "__main__":
--- fibo.rb
+++ fibo.rb
@@ -1,16 +1,48 @@
 class FiboTest
-	def fibo(n)
-		if n==0 || n==1 then
-			1
-		else
-			fibo(n-1) + fibo(n-2)
-		end
+	attr_accessor :cache_instance	# !important
+
+	def initialize()
+		@cache_instance = {}	# instance variable
 	end
+
+	@@cache = {}	# class variable
+	# class method
+	def self.fibo(n)	
+		if @@cache.has_key?(n) then
+			return @@cache[n]
+		end
+		if n == 0
+			return 0
+		end
+		if n == 1
+			return 1
+		end
+		@@cache[n] = self.fibo(n-1) + self.fibo(n-2)
+		return @@cache[n]
+	end
+
+	#instance method
+	# def fibo(n)
+	# 	if @cache_instance.has_key?(n)
+	# 		return @cache_instance[n]
+	# 	end
+	# 	if n == 0
+	# 		return 0
+	# 	end
+	# 	if n == 1
+	# 		return 1
+	# 	end
+	# 	@cache_instance[n] = fibo(n-1) + fibo(n-2)
+	# 	return @cache_instance[n]
+	# end
+
 end
 
 fibo = FiboTest.new
+puts fibo.cache_instance
 
 t1 = Time.now
-puts fibo.fibo(35)
+# puts fibo.fibo(40)	# using instance method
+puts FiboTest.fibo(40)	# using class method
 t2 = Time.now
 puts "Time elapsed #{t2-t1}"
