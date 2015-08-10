07def8853289e325180b760fbebd59deb8b15d29
Alan Cao <alannesta@sina.com>
2015-08-10 13:45:08 -0400

    make the module an npm module that could be used by other projects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec;
 var Q = require('q');
+var path = require('path');
 var jobQueue = [];
 var benchmark_result = {};
 
@@ -12,49 +13,53 @@ var config = {
 	python: {
 		command: 'python fibo.py ' + fibonacci,
 		target: 'fibo.py',
-		cwd: './src'
+		cwd: path.join(__dirname, 'src')
 	},
 	ruby: {
 		command: 'ruby fibo.rb ' + fibonacci,
 		target: 'fibo.rb',
-		cwd: './src'
+		cwd: path.join(__dirname, 'src')
 	},
 	javascript: {
 		command: 'node fibo.js ' + fibonacci,
 		target: 'fibo.js',
-		cwd: './src'
+		cwd: path.join(__dirname, 'src')
 	},
 	java: {
 		command: 'javac -d ./ ../src/Fibonacci.java && java -cp ./ Fibonacci ' + fibonacci,
 		target: '',
-		cwd: './bin'
+		cwd: path.join(__dirname, 'bin')
 	},
 	c: {
 		command: 'gcc -O2 -o ./fibo_c ../src/fibo.c && ./fibo_c ' + fibonacci,
 		target: '',
-		cwd: './bin'
+		cwd: path.join(__dirname, 'bin')
 	}
 };
 
-for (var key in config) {
-	jobQueue.push(createTask(config[key]));
-}
+
+
+var kickStart = function() {
+	for (var key in config) {
+		jobQueue.push(createTask(config[key]));
+	}
 //jobQueue[0]().then(jobQueue[1]).then(jobQueue[2]).then(jobQueue[3]).then(jobQueue[4]);
 //jobQueue[4]();
 
-var start = Q.defer();
-var task_in_progress  = Q.when(start.promise);
+	var start = Q.defer();
+	var task_in_progress  = Q.when(start.promise);
 
 // it would be better to use async for brevity
-jobQueue.forEach(function(nextTask) {
-	task_in_progress = task_in_progress.then(nextTask);
-});
+	jobQueue.forEach(function(nextTask) {
+		task_in_progress = task_in_progress.then(nextTask);
+	});
 
-task_in_progress.then(function() {
-	console.log(benchmark_result);
-});
+	task_in_progress.then(function() {
+		console.log(benchmark_result);
+	});
 
-start.resolve('gg');
+	start.resolve('gg');
+};
 
 function createTask(task) {
 	return function() {
@@ -94,4 +99,7 @@ function parseLang(stdout) {
 	return language_reg.exec(stdout)[0];
 }
 
+kickStart();
+
+module.exports = kickStart;
 
